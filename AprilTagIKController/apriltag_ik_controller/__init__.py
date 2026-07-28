137dbba5a3770f5afd42263aa5dafee6f6c1a5cb
Waldfee <waldfeeolladie+gitlab@gmail.com>
2026-07-28 19:08:17 +0200

    remove apirl tag ik solver package

--- AprilTagIKController/apriltag_ik_controller/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-AprilTag IK Controller Package for Kinova Gen3 Arm.
-"""
-
-from .ik_solver import KinovaIKSolver
-from .tag_ik_node import TagIKNode
-
-__all__ = ['KinovaIKSolver', 'TagIKNode']
